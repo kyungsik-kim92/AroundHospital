ab99902893bc79cd9528c840729e10ed5ce3b1c4
Kim Kyungsik <ksiki1@naver.com>
2024-05-04 00:16:27 +0900

    BindingAdapter 추가

--- app/src/main/java/com/example/aroundhospital/BindingAdapter.kt
+++ app/src/main/java/com/example/aroundhospital/BindingAdapter.kt
@@ -1,8 +1,12 @@
 package com.example.aroundhospital
 
 import android.animation.Animator
+import android.view.View
 import androidx.databinding.BindingAdapter
 import com.airbnb.lottie.LottieAnimationView
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
+import net.daum.mf.map.api.MapView
 
 @BindingAdapter("onLottieAnimateState")
 fun LottieAnimationView.onLottieAnimateState(state: Function1<LottieAnimateState, Unit>?) {
@@ -31,4 +35,82 @@ sealed interface LottieAnimateState {
     object End : LottieAnimateState
     object Cancel : LottieAnimateState
     object Repeat : LottieAnimateState
+}
+
+@BindingAdapter("onPOIItemEventListener")
+fun MapView.onPOIItemEventListener(f: Function1<POIItemEventType, Unit>?) {
+    setPOIItemEventListener(object : MapView.POIItemEventListener {
+        override fun onPOIItemSelected(p0: MapView?, p1: MapPOIItem?) {
+            f?.invoke(POIItemEventType.Selected(p1))
+        }
+
+        override fun onCalloutBalloonOfPOIItemTouched(p0: MapView?, p1: MapPOIItem?) {
+        }
+
+        override fun onCalloutBalloonOfPOIItemTouched(
+            p0: MapView?,
+            p1: MapPOIItem?,
+            p2: MapPOIItem.CalloutBalloonButtonType?
+        ) {
+
+        }
+
+        override fun onDraggablePOIItemMoved(p0: MapView?, p1: MapPOIItem?, p2: MapPoint?) {
+        }
+    })
+}
+
+sealed interface POIItemEventType {
+    data class Selected(val item: MapPOIItem?) : POIItemEventType
+}
+
+
+@BindingAdapter("onMapViewEventListener")
+fun MapView.onMapViewEventListener(f: Function1<MapViewEventType, Unit>?) {
+
+    val eventListener = object : MapView.MapViewEventListener {
+        override fun onMapViewInitialized(p0: MapView?) {
+        }
+
+        override fun onMapViewCenterPointMoved(p0: MapView?, p1: MapPoint?) {
+
+        }
+
+        override fun onMapViewZoomLevelChanged(p0: MapView?, p1: Int) {
+        }
+
+        override fun onMapViewSingleTapped(p0: MapView?, p1: MapPoint?) {
+        }
+
+        override fun onMapViewDoubleTapped(p0: MapView?, p1: MapPoint?) {
+        }
+
+        override fun onMapViewLongPressed(p0: MapView?, p1: MapPoint?) {
+        }
+
+        override fun onMapViewDragStarted(p0: MapView?, p1: MapPoint?) {
+            f?.invoke(MapViewEventType.DragStarted)
+        }
+
+        override fun onMapViewDragEnded(p0: MapView?, p1: MapPoint?) {
+        }
+
+        override fun onMapViewMoveFinished(p0: MapView?, p1: MapPoint?) {
+        }
+    }
+
+    setMapViewEventListener(eventListener)
+}
+
+sealed interface MapViewEventType {
+
+    object DragStarted : MapViewEventType
+
+    data class MapCenterChanged(val mapPoint: MapPoint) : MapViewEventType
+}
+
+
+@BindingAdapter("isVisible")
+fun View.isVisible(isVisible: Boolean) {
+    this.visibility = if (isVisible) View.VISIBLE else View.GONE
 }
