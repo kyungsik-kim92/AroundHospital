53202536e639185537e6e1d2f2b7947f449660a7
Kim Kyungsik <ksiki1@naver.com>
2024-05-03 20:35:30 +0900

    ViewExt 추가

---
+++ app/src/main/java/com/example/aroundhospital/ViewExt.kt
@@ -0,0 +1,28 @@
+package com.example.aroundhospital
+
+import android.content.Context
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.aroundhospital.splash.SplashFragmentDirections
+
+fun Fragment.showToast(message: String) {
+    Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+}
+
+
+fun Fragment.routeHomeFragment() {
+    findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment())
+}
+
+fun View.hidePOIInfoContainer(context : Context) {
+    this.visibility = View.GONE
+    this.startAnimation(AnimationUtils.loadAnimation(context, R.anim.slide_down))
+}
+
+fun View.showPOIInfoContainer(context : Context) {
+    this.visibility = View.VISIBLE
+    this.startAnimation(AnimationUtils.loadAnimation(context, R.anim.slide_up))
+}
