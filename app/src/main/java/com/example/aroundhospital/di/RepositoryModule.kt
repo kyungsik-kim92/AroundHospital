54831dca31e32b5910ccc71508d7bbaefac66dd6
Kim Kyungsik <ksiki1@naver.com>
2024-05-04 00:54:31 +0900

    RepositoryModule 생성

---
+++ app/src/main/java/com/example/aroundhospital/di/RepositoryModule.kt
@@ -0,0 +1,40 @@
+package com.example.aroundhospital.di
+
+
+import com.example.aroundhospital.data.remote.KakaoRemoteDataSource
+import com.example.aroundhospital.data.remote.KakaoRemoteDataSourceImpl
+import com.example.aroundhospital.data.repo.KakaoRepository
+import com.example.aroundhospital.data.repo.KakaoRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+
+    @Binds
+    @Singleton
+    abstract fun provideKakaoRepository(kakaoRepositoryImpl: KakaoRepositoryImpl): KakaoRepository
+
+//    @Binds
+//    @Singleton
+//    abstract fun bindBookmarkRepository(
+//        bookmarkRepositoryImpl: BookmarkRepositoryImpl
+//    ): BookmarkRepository
+//
+
+    @Binds
+    @Singleton
+    abstract fun provideKakaoRemoteDataSource(kakaoRemoteDataSourceImpl: KakaoRemoteDataSourceImpl): KakaoRemoteDataSource
+
+//    @Binds
+//    @Singleton
+//    abstract fun bindBookmarkLocalDataSource(
+//        bookmarkLocalDataSourceImpl: BookmarkLocalDataSourceImpl
+//    ): BookmarkLocalDataSource
+
+}
