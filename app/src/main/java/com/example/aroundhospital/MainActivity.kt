6e6b40688a856efb5781247ab9e93e7b0319ad13
Kim Kyungsik <ksiki1@naver.com>
2024-05-04 12:36:26 +0900

    BookmarkFragment/BookmarkViewModel,BookmarkViewState

--- app/src/main/java/com/example/aroundhospital/MainActivity.kt
+++ app/src/main/java/com/example/aroundhospital/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.aroundhospital
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,4 +13,3 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
-}
