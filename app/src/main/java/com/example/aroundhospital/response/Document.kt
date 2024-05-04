7e768a6bfd828f8c404a8883cfe1058059bddf7d
Kim Kyungsik <ksiki1@naver.com>
2024-05-04 12:07:16 +0900

    Document 파일 수정

--- app/src/main/java/com/example/aroundhospital/response/Document.kt
+++ app/src/main/java/com/example/aroundhospital/response/Document.kt
@@ -1,19 +1,36 @@
 package com.example.aroundhospital.response
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import net.daum.mf.map.api.MapPOIItem
 
+@Entity(tableName = "book")
 data class Document(
+    @ColumnInfo(name = "address_name")
     val address_name: String,
+    @ColumnInfo(name = "category_group_code")
     val category_group_code: String,
+    @ColumnInfo(name = "category_group_name")
     val category_group_name: String,
+    @ColumnInfo(name = "category_name")
     val category_name: String,
+    @ColumnInfo(name = "distance")
     val distance: String,
+    @PrimaryKey
+    @ColumnInfo(name = "id")
     val id: String,
+    @ColumnInfo(name = "phone")
     val phone: String,
+    @ColumnInfo(name = "place_name")
     val place_name: String,
+    @ColumnInfo(name = "place_url")
     val place_url: String,
+    @ColumnInfo(name = "road_address_name")
     val road_address_name: String,
+    @ColumnInfo(name = "x")
     val x: String,
+    @ColumnInfo(name = "y")
     val y: String
 )
 
