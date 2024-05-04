9289a8e9587101d7b016e889c9c47c23c429af6d
Kim Kyungsik <ksiki1@naver.com>
2024-05-04 15:58:34 +0900

    Appkey 추가

--- app/src/main/java/com/example/aroundhospital/App.kt
+++ app/src/main/java/com/example/aroundhospital/App.kt
@@ -1,9 +1,15 @@
 package com.example.aroundhospital
 
 import android.app.Application
+import com.kakao.vectormap.KakaoMapSdk
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
 class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        KakaoMapSdk.init(this,"5d205f362e46b908c25920b3c0d459ed")
+    }
 }
