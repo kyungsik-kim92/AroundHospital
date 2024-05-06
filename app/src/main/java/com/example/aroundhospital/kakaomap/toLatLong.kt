d0a1c7d5ad388ea115e58f0b6b238c29727fc011
Kim Kyungsik <ksiki1@naver.com>
2024-05-06 23:02:24 +0900

    확장함수 이동

---
+++ app/src/main/java/com/example/aroundhospital/kakaomap/toLatLong.kt
@@ -0,0 +1,12 @@
+package com.example.aroundhospital.kakaomap
+
+import android.location.Location
+import com.example.domain.model.KakaoMapInfo
+import com.kakao.vectormap.LatLng
+import com.kakao.vectormap.label.LabelOptions
+
+fun Location.toLatLng() =
+   LatLng.from(latitude, longitude)
+
+fun KakaoMapInfo.toKakaoMapLabelOption(): LabelOptions =
+    LabelOptions.from(id, LatLng.from(y.toDouble(), x.toDouble())).setTag(this)
