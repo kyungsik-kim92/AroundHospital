ddcd0245abd2cda0e7e0f54edc4c9e2f606f86e5
Kim Kyungsik <ksiki1@naver.com>
2024-05-06 23:01:27 +0900

    필요없는 파일 삭제

--- domain/src/main/java/com/example/domain/exception/Exception.kt
+++
@@ -1,8 +0,0 @@
-package com.example.domain.exception
-
-class NetworkFailureException(message: String? = "") : Exception(message)
-
-class SearchErrorException(message: String? = "") : Exception(message)
-
-
-class PermissionDeniedException(message: String? = "") : Exception(message)
