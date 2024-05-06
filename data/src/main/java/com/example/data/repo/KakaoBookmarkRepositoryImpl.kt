227045cd48f8863554b3b2a60761ef8fe3319334
Kim Kyungsik <ksiki1@naver.com>
2024-05-06 23:06:34 +0900

    RpositoryImpl

--- data/src/main/java/com/example/data/repo/KakaoBookmarkRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repo/KakaoBookmarkRepositoryImpl.kt
@@ -1,21 +1,24 @@
 package com.example.data.repo
 
+import com.example.data.mapper.toDocument
+import com.example.data.mapper.toKakaoMapInfo
+import com.example.data.source.local.BookmarkLocalDataSource
 import com.example.domain.model.KakaoMapInfo
 import com.example.domain.repo.KakaoBookmarkRepository
-import com.example.domain.source.local.BookmarkLocalDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class KakaoBookmarkRepositoryImpl @Inject constructor(private val bookmarkLocalDataSource: BookmarkLocalDataSource) :
     KakaoBookmarkRepository {
     override val getAll: Flow<List<KakaoMapInfo>>
-        get() = bookmarkLocalDataSource.getAll
+        get() = bookmarkLocalDataSource.getAll.map { list -> list.map { item -> item.toKakaoMapInfo() } }
 
     override suspend fun delete(entity: KakaoMapInfo) {
-       bookmarkLocalDataSource.delete(entity)
+        bookmarkLocalDataSource.delete(entity.toDocument())
     }
 
     override suspend fun insert(entity: KakaoMapInfo) {
-        bookmarkLocalDataSource.insert(entity)
+        bookmarkLocalDataSource.insert(entity.toDocument())
     }
 }
--- data/src/main/java/com/example/data/repo/KakaoRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repo/KakaoRepositoryImpl.kt
@@ -1,14 +1,15 @@
 package com.example.data.repo
 
+import com.example.data.mapper.toKakaoSearch
 import com.example.domain.model.KakaoSearch
 import com.example.domain.repo.KakaoRepository
-import com.example.domain.source.remote.KakaoRemoteDataSource
+import com.example.data.source.remote.KakaoRemoteDataSource
 import javax.inject.Inject
 
 class KakaoRepositoryImpl @Inject constructor(private val kakaoRemoteDataSource: KakaoRemoteDataSource) :
     KakaoRepository {
     override suspend fun getSearchHospital(x: String, y: String): KakaoSearch {
-        return kakaoRemoteDataSource.getSearchHospital(x, y)
+        return kakaoRemoteDataSource.getSearchHospital(x, y).toKakaoSearch()
 
     }
 
