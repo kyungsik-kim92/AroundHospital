d17df358057a6dccae22a65a5c4de114d8b7bce5
Kim Kyungsik <ksiki1@naver.com>
2024-05-06 23:05:07 +0900

    BookmarkLocalDataSource

--- data/src/main/java/com/example/data/source/local/BookmarkLocalDataSource.kt
+++ data/src/main/java/com/example/data/source/local/BookmarkLocalDataSource.kt
@@ -1,13 +1,14 @@
-package com.example.aroundhospital.data.local
+package com.example.data.source.local
 
 import com.example.data.api.response.Document
+import com.example.domain.model.KakaoMapInfo
 import kotlinx.coroutines.flow.Flow
 
 interface BookmarkLocalDataSource {
-
     val getAll: Flow<List<Document>>
 
     suspend fun delete(entity: Document)
 
     suspend fun insert(entity: Document)
+
 }
--- data/src/main/java/com/example/data/source/local/BookmarkLocalDataSourceImpl.kt
+++ data/src/main/java/com/example/data/source/local/BookmarkLocalDataSourceImpl.kt
@@ -1,24 +1,21 @@
 package com.example.data.source.local
 
+import com.example.data.api.response.Document
 import com.example.data.room.BookmarkDao
-import com.example.domain.model.KakaoMapInfo
-import com.example.domain.source.local.BookmarkLocalDataSource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class BookmarkLocalDataSourceImpl @Inject constructor(private val bookmarkDao: BookmarkDao) :
     BookmarkLocalDataSource {
-    override val getAll: Flow<List<KakaoMapInfo>>
-        = bookmarkDao.getAll()
+    override val getAll: Flow<List<Document>> = bookmarkDao.getAll()
 
-    override suspend fun delete(entity: KakaoMapInfo) {
+    override suspend fun delete(entity: Document) {
 
-         bookmarkDao.delete(entity)
+        bookmarkDao.delete(entity)
     }
 
-    override suspend fun insert(entity: KakaoMapInfo) {
-        TODO("Not yet implemented")
+    override suspend fun insert(entity: Document) {
+        bookmarkDao.insert(entity)
     }
 
 }
